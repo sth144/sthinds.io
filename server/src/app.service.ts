436c2fd33c748c698889fe6e240de7de6c4132dc
Sean Hinds <sthinds@sthinds.pc>
2022-06-16 23:25:10 -0500

    Progress on pipeline env

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
+// TODO: get rid of this or use it to route to other services
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
 }
- 
