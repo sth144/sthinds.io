365235f007d74a570333cb5f72369421082b4d96
sth144 <sthinds144@gmail.com>
2025-09-20 17:12:45 -0500

    Auth

--- client/src/network/graphql.service.ts
+++ client/src/network/graphql.service.ts
@@ -1,23 +1,37 @@
 import { ApolloClient, InMemoryCache, HttpLink, from } from "@apollo/client";
 import { onError, ErrorResponse } from "@apollo/client/link/error";
+import { setContext } from "@apollo/client/link/context";
 
 const serverProtocol = window.location.protocol;
 const serverHost = window.location.hostname;
 const serverPortStr = serverHost.includes("localhost") ? ":3200" : "";
 
+const httpLink = new HttpLink({
+  uri: `${serverProtocol}//${serverHost}${serverPortStr}/graphql`,
+});
+
+const authLink = setContext((_: unknown, { headers }: any) => {
+  const token = JSON.parse(localStorage.getItem("appState") as string)
+    .authentication.token;
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+console.log("Auth Link:", authLink);
 const GraphQLService = new ApolloClient({
   cache: new InMemoryCache(),
   link: from([
-    onError((errorResponse: ErrorResponse) => {
-      if (errorResponse.graphQLErrors) {
-        errorResponse.graphQLErrors.forEach((error) => {
-          alert(`GraphQL error ${error.message}`);
-        });
-      }
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.forEach((err) =>
+          console.error(`GraphQL error: ${err.message}`)
+        );
+      if (networkError) console.error(networkError);
     }),
-    new HttpLink({
-      uri: `${serverProtocol}//${serverHost}${serverPortStr}/graphql`,
-    }),
+    authLink.concat(httpLink),
   ]),
 });
 
