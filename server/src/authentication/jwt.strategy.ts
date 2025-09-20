b443f236e0d0af9ec16bddb9a1f46a2094f20c34
sth144 <sthinds144@gmail.com>
2025-09-20 16:32:55 -0500

    Deploy

--- server/src/authentication/jwt.strategy.ts
+++ server/src/authentication/jwt.strategy.ts
@@ -28,7 +28,6 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
       // TODO: You could add a function to the authService to verify the claims of the token:
       // i.e. does the user still have the roles that are claimed by the token
       //const validClaims = await this.authService.verifyTokenClaims(payload);
-
       //if (!validClaims)
       //    return done(new UnauthorizedException('invalid token claims'), false);
 
